7f5872bf71ac2d675bbd9d09857975ecb28e7020
sumit0593 <sumit0593>
2023-03-25 14:10:52 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,16 @@ import '../styles/App.css';
 
 const App = () => {
 
-  const [isDarkMode, setIsDarkMode] = useState();
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
+  const handleMode=()=>{
+    setIsDarkMode(!isDarkMode);
+    
+  }
   return (
-    <div id="main">
-      <h1></h1>
-      <button></button>
+    <div id="main" style={{background: isDarkMode ? "rgb(255, 255, 255)" : "rgb(0, 0, 0)"}}>
+      <h1 style={{color: isDarkMode ? "rgb(0, 0, 0)" : "rgb(255, 255, 255)"}}>{isDarkMode ? "Light Mode Activated" :"Dark Mode Activated"}</h1>
+      <button onClick={handleMode}> {isDarkMode ? "Switch to Dark Mode" : "Switch to Light Mode"}</button>
     </div>
   )
 }
